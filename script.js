abd0b8ec1f41e903729aede6bc6827dcf58906a6
jori-fm <iwillkillutonigh@gmail.com>
2025-08-06 07:01:30 -0500

    Edit indexhtml, implement script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add hover effects to releases
+document.querySelectorAll('.release').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        item.style.transform = 'translateY(-5px)';
+    });
+    item.addEventListener('mouseleave', () => {
+        item.style.transform = 'none';
+    });
+});
